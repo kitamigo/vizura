16f0955f6fcca5c5f7c37789550711799d9a5df3
Kita Henry <nrh@outlook.co.nz>
2026-05-19 14:29:42 +1200

    draft
    draft code for development in class

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 require('dotenv').config();
-const app = require('./src/app');
 require('./src/config/db');
-
+const app = require('./src/app');
 const PORT = process.env.PORT || 5000;
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 app.listen(PORT, () => {
     console.log(`Vizura server running on port ${PORT}`);
