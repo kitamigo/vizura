329917ec80584a792c702025c4f7ccfa803197ed
Kita Henry <nrh@outlook.co.nz>
2026-06-22 18:07:23 +1200

    Analytics frontend draft
    Draft of frontend analytics

---
+++ client/src/components/ui/Button.jsx
@@ -0,0 +1,12 @@
+import { Button } from "flowbite-react";
+
+export function Component() {
+  return (
+    <div className="flex flex-wrap gap-2">
+      <Button className="bg-gradient-to-br from-purple-600 to-blue-500 text-white hover:bg-gradient-to-bl focus:ring-blue-300 dark:focus:ring-blue-800" size="xl"></Button>
+    </div>
+  );
+}
+
+
+export default Component
